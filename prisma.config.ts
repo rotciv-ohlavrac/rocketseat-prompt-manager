dd4fa7162634e0f74e42afdeaa2310aa228bbb0d
¨gelado¨ <victor.rodrigues.carvalho.santos@gmail.com>
2026-05-05 11:03:24 -0300

    feat: :wrench: config prisma

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+const url = process.env.DATABASE_URL!;
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: url,
+  },
+});
