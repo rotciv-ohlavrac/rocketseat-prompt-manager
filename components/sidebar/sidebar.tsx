3d5b6f26780150c582dc095b49733886065ff19d
¨gelado¨ <victor.rodrigues.carvalho.santos@gmail.com>
2026-05-06 14:31:37 -0300

    feat: :sparkles: Add new components Sidebar and SidebarContent

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,11 @@
+import { prismaClient } from '@/lib/prisma';
+import { SidebarContent } from './sidebar-content';
+
+async function Sidebar() {
+  const prompts = await prismaClient.prompt.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+  return <SidebarContent />;
+}
+
+export { Sidebar };
