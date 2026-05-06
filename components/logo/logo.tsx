2c27f04ffcd6323a9f5d87d8c97331bf6e28e051
¨gelado¨ <victor.rodrigues.carvalho.santos@gmail.com>
2026-05-06 14:32:00 -0300

    feat: :sparkles: Add new component Logo

---
+++ components/logo/logo.tsx
@@ -0,0 +1,16 @@
+import { MessageSquare } from 'lucide-react';
+import Link from 'next/link';
+
+function Logo() {
+  return (
+    <Link
+      href="/"
+      className="flex items-center gap-2 hover:text-accent-600 rounded-lg transition-colors"
+    >
+      <MessageSquare />
+      <span className="text-lg font-semibold">PROMPTS</span>
+    </Link>
+  );
+}
+
+export { Logo };
