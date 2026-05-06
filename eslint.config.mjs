f748f0125ffec5c5572745f64389810892916e56
¨gelado¨ <victor.rodrigues.carvalho.santos@gmail.com>
2026-05-06 14:47:06 -0300

    chore: :wrench: update eslint.config.mjs to ignore postres-data folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
+  {
+    ignores: [
+      'postgres_data/**',
+      '**/postgres_data/**',
+      'dist/**',
+      'node_modules/**',
+    ],
+  },
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
