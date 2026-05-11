1d5b9e5d82aa2b2e703cdbcc75eedaeaf7aa4f2e
¨gelado¨ <victor.rodrigues.carvalho.santos@gmail.com>
2026-05-11 20:24:27 -0300

    feat: :sparkles: update prisma version

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '../app/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const prismaClient = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+
+const prismaClient = new PrismaClient({ adapter });
 
 export { prismaClient };
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,15 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
-
-const url = process.env.DATABASE_URL!;
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
+
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
-  engine: "classic",
+
   datasource: {
-    url: url,
+    url: env('DATABASE_URL'),
   },
 });
