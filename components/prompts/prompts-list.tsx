11ebf1eb05efe238233b5d8f02d39458697910b9
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2026-09-09 01:46:37 -0300

    feat: :lipstick: add some animations to prompts-list

--- components/prompts/prompts-list.tsx
+++ components/prompts/prompts-list.tsx
@@ -1,5 +1,6 @@
 import { PromptSummary } from '@/core/domain/prompts/prompts.entity';
 import { PromptsCard } from './prompts-card';
+import { motion } from 'motion/react';
 
 type PromptsListProps = {
   prompts: PromptSummary[];
@@ -11,7 +12,18 @@ function PromptsList({ prompts }: PromptsListProps) {
       <PromptsCard key={prompt.id} prompt={prompt} />
     ));
   };
-  return <ul>{renderPrompts()}</ul>;
+  return (
+    <motion.ul
+      className="space-y-2"
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -20 }}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
+      layout
+    >
+      {renderPrompts()}
+    </motion.ul>
+  );
 }
 
 export { PromptsList };
