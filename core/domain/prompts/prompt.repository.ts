33a181d056ac2f75a2e519236464f8fcf54d2c1a
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2026-09-09 01:48:57 -0300

    feat: :sparkles: add new methods to PromptRepository interface

--- core/domain/prompts/prompt.repository.ts
+++ core/domain/prompts/prompt.repository.ts
@@ -4,6 +4,9 @@ import { Prompt } from './prompts.entity';
 export interface PromptRepository {
   findMany(): Promise<Prompt[]>;
   findByTitle(title: string): Promise<Prompt | null>;
+  findById(id: string): Promise<Prompt | null>;
   searchMany(term: string): Promise<Prompt[]>;
   create(prompt: CreatePromptDTO): Promise<void>;
+  update(id: string, data: Partial<CreatePromptDTO>): Promise<Prompt>;
+  delete(id: string): Promise<void>;
 }
