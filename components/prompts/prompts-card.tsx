fa9b1e752a844d57be54aba362f2e5b537e2e158
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2026-09-09 01:46:00 -0300

    feat: :sparkles: handle prompt delete and add animations to prompts-card

--- components/prompts/prompts-card.tsx
+++ components/prompts/prompts-card.tsx
@@ -1,13 +1,64 @@
+'use client';
+import { useState } from 'react';
 import { PromptSummary } from '@/core/domain/prompts/prompts.entity';
+import { Button } from '../ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../ui/alert-dialog';
+import { Trash as DeleteIcon, Loader2 as LoadingIcon } from 'lucide-react';
 import Link from 'next/link';
+import { toast } from 'sonner';
+import { deletePromptAction } from '@/app/actions/prompt.actions';
+import { motion } from 'motion/react';
+import { useRouter } from 'next/navigation';
 
 type PromptsCardProps = {
   prompt: PromptSummary;
 };
 
 function PromptsCard({ prompt }: PromptsCardProps) {
+  const router = useRouter();
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+
+  async function handleDelete() {
+    setIsDeleting(true);
+
+    try {
+      const result = await deletePromptAction(prompt.id);
+      if (!result.success) {
+        toast.error(result.message);
+        return;
+      }
+      toast.success(result.message);
+      router.refresh();
+    } catch (error) {
+      const _error = error as Error;
+      toast.error(_error.message);
+    } finally {
+      setIsDeleting(false);
+    }
+  }
+
   return (
-    <li className="p-3 rounded-lg transition-all duration-200 group relative hover:bg-gray-700">
+    <motion.li
+      className="p-3 rounded-lg transition-all duration-200 group relative hover:bg-gray-700"
+      aria-label={prompt.title}
+      initial={{ opacity: 1, height: 'auto' }}
+      exit={{
+        opacity: 0,
+        height: 0,
+        marginBottom: 0,
+        transition: { duration: 0.3, ease: 'easeInOut' },
+      }}
+    >
       <header className="flex items-start justify-between">
         <Link href={`/${prompt.id}`} prefetch className="flex-1 min-w-0">
           <h3 className="font-medium text-sm text-white group-hover:text-accent-300 transition-colors">
@@ -18,8 +69,39 @@ function PromptsCard({ prompt }: PromptsCardProps) {
             {prompt.content}
           </p>
         </Link>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              variant="icon"
+              size="icon"
+              title="Remover Prompt"
+              aria-label="Remover Prompt"
+            >
+              <DeleteIcon className="w-3 h-3" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Remover prompt</AlertDialogTitle>
+              <AlertDialogDescription>
+                Tem certeza que deseja remover esse prompt? Esta ação não pode
+                ser desfeita.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDelete} disabled={isDeleting}>
+                {isDeleting ? (
+                  <LoadingIcon className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  'Confirmar remoção'
+                )}
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </header>
-    </li>
+    </motion.li>
   );
 }
 
